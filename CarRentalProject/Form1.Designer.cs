179bf5d95cb5aeb3b2e7fd7223091d00a36e2f10
Filip <vixu19@gmail.com>
2022-07-10 00:40:39 +0200

    Initial upload

---
+++ CarRentalProject/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace CarRentalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.signupButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(19, 18);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(129, 23);
+            this.loginTextBox.TabIndex = 0;
+            this.loginTextBox.TextChanged += new System.EventHandler(this.loginTextBox_TextChanged);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(19, 47);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(129, 23);
+            this.passwordTextBox.TabIndex = 1;
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.passwordTextBox_TextChanged);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(32, 76);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(104, 23);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // signupButton
+            // 
+            this.signupButton.Location = new System.Drawing.Point(32, 105);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Size = new System.Drawing.Size(104, 23);
+            this.signupButton.TabIndex = 3;
+            this.signupButton.Text = "Sign up";
+            this.signupButton.UseVisualStyleBackColor = true;
+            this.signupButton.Click += new System.EventHandler(this.signupButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.signupButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button signupButton;
+    }
+}
+
---
+++ CarRentalProject/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CarRentalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void passwordTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string[] data = new string[100];
+            string line;
+            int i = 0;
+            string path = @"C:\Users\vixu1\source\repos\CarRentalProject\logins.txt";
+            
+            //Make filestream for reading the file
+            FileStream fsRead = new FileStream(path, FileMode.Open, FileAccess.Read);
+            
+            using (StreamReader sr = new StreamReader(fsRead))
+            {
+                //Write contents of file to data array
+                while ((line = sr.ReadLine()) != null)
+                {
+                    data[i] = line;
+                    i += 1;
+                }
+            }
+
+            for(int j = 0; j<i; j += 2)
+            {
+                //Check if login and password match data
+                if (loginTextBox.Text == data[j] && passwordTextBox.Text == data[j + 1])
+                {
+                    MessageBox.Show("Logged-in!", "Logged-in!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                //Check if login matches the data, but the password is different
+                else if (loginTextBox.Text == data[j] && passwordTextBox.Text != data[j + 1])
+                {
+                    MessageBox.Show("Password doesnt match the login!", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            //No user of that name found
+            MessageBox.Show("User of that login already exists!", "Login Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        //Registration process
+        private void signupButton_Click(object sender, EventArgs e)
+        {
+            //Check if text boxes are empty
+            if (loginTextBox.Text == "" || passwordTextBox.Text == "")
+            {
+                MessageBox.Show("Login or password is missing!", "No Data!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string[] data = new string[100];
+            //Make the path for the logins file
+            string path = @"C:\Users\vixu1\source\repos\CarRentalProject\logins.txt";
+            FileStream fsRead = new FileStream(path, FileMode.Open, FileAccess.Read);
+            string line;
+            int i = 0;
+
+            //Write contents of file to data array
+            using (StreamReader sr = new StreamReader(fsRead))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    data[i] = line;
+                    i += 1;
+
+                }
+            }
+            fsRead.Close();
+            
+            FileStream fsWrite = new FileStream(path, FileMode.Append, FileAccess.Write);
+            using (StreamWriter sw = new StreamWriter(fsWrite)) 
+            { 
+                //Check if the login already exists
+                for (int j = 0; j < i; j += 2)
+                {
+                    if (loginTextBox.Text == data[j])
+                    {
+                        MessageBox.Show("This login already exists!", "Registery Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                //Write login and password to file
+                sw.WriteLine(loginTextBox.Text);
+                sw.WriteLine(passwordTextBox.Text);
+                MessageBox.Show("Successfully registered!", "Registered!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            fsWrite.Close();
+        } 
+    }
+}
